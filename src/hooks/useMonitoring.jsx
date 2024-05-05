a14168921d2dbe0e161d8c36d8ed9f83f25d7de6
Reinhardt Botha <botha.wr@gmail.com>
2024-05-04 20:20:35 -0700

    chore: optimize custom hook

--- src/hooks/useMonitoring.jsx
+++ src/hooks/useMonitoring.jsx
@@ -1,19 +1,71 @@
 import React from "react";
 
 const useMonitoring = () => {
-  const [cpu, setCpu] = React.useState();
-  const [gpu, setGpu] = React.useState();
-  const [ram, setRam] = React.useState();
-  const [kraken, setKraken] = React.useState();
-  const [cpuWatts, setCpuWatts] = React.useState(
-    Array.from({ length: 60 }, (_, index) => index + 1)
-  );
-  const [gpuWatts, setGpuWatts] = React.useState(
-    Array.from({ length: 60 }, (_, index) => index + 1)
-  );
+  const [monitoringData, setMonitoringData] = React.useState({
+    cpu: null,
+    gpu: null,
+    ram: null,
+    kraken: null,
+    cpuWatts: Array.from({ length: 60 }, (_, index) => index + 1),
+    gpuWatts: Array.from({ length: 60 }, (_, index) => index + 1),
+  });
 
   React.useEffect(() => {
     const nzxtDefaults = window.nzxt?.v1;
+
+    const updateMonitoringData = (data) => {
+      const { cpus, gpus, ram, kraken } = data;
+
+      const cpu = cpus.pop();
+
+      let gpu;
+
+      if (gpus.length > 1) {
+        const [mainGpu] = gpus.filter(
+          (card) => !card.name.includes("Graphics")
+        );
+        gpu = mainGpu;
+      } else {
+        gpu = gpus.pop();
+      }
+
+      setMonitoringData((prevData) => ({
+        ...prevData,
+        cpu: {
+          // name: cpu?.name,
+          load: Number(((cpu?.load ?? 1) * 100).toFixed(0)) ?? 0,
+          temperature: Number(cpu?.temperature?.toFixed(0)),
+          // maxTemperature: Number(cpu?.maxTemperature),
+          frequency: Number(cpu?.frequency),
+          fan: Number(cpu?.fanSpeed),
+        },
+        cpuWatts: [
+          ...prevData.cpuWatts.slice(1),
+          Number(cpu?.power.toFixed(0)),
+        ],
+        gpu: {
+          // name: gpu?.name,
+          load: Number(((gpu?.load ?? 1) * 100).toFixed(0)) ?? 0,
+          temperature: Number(gpu?.temperature?.toFixed(0)),
+          // maxTemperature: Number(gpu?.maxTemperature),
+          frequency: Number(gpu?.frequency),
+        },
+        gpuWatts: [
+          ...prevData.gpuWatts.slice(1),
+          Number(gpu?.power.toFixed(0)),
+        ],
+        ram: {
+          inUse: Math.round((ram.inUse ?? 1) / 1024),
+          inUsePercent: Math.round(
+            ((ram.inUse ?? 1) / (ram.totalSize ?? 1)) * 100
+          ),
+          totalSize: Math.round((ram.totalSize ?? 1) / 1024),
+        },
+        kraken: {
+          temperature: Number(kraken?.liquidTemperature.toFixed(0)),
+        },
+      }));
+    };
 
     window.nzxt = {
       v1: {
@@ -21,75 +73,12 @@ const useMonitoring = () => {
         height: nzxtDefaults?.height ?? 0,
         shape: nzxtDefaults?.shape ?? "circle",
         targetFps: nzxtDefaults?.targetFps ?? 10,
-        onMonitoringDataUpdate: (data) => {
-          const { cpus, gpus, ram, kraken } = data;
-
-          const cpu = cpus.pop();
-
-          let gpu;
-
-          if (gpus.length > 1) {
-            const [mainGpu] = gpus.filter(
-              (card) => !card.name.includes("Graphics")
-            );
-            gpu = mainGpu;
-          } else {
-            gpu = gpus.pop();
-          }
-
-          setCpu({
-            name: cpu?.name,
-            load: Number(((cpu?.load ?? 1) * 100).toFixed(0)) ?? 0,
-            temperature: Number(cpu?.temperature?.toFixed(0)),
-            maxTemperature: Number(cpu?.maxTemperature),
-            frequency: Number(cpu?.frequency),
-            fan: Number(cpu?.fanSpeed),
-            watts: Number(cpu?.power.toFixed(0)),
-          });
-
-          setCpuWatts((prevCpuWatts) => {
-            const newCpuWatts = [
-              ...prevCpuWatts.slice(1),
-              Number(cpu?.power.toFixed(0)),
-            ];
-            return newCpuWatts;
-          });
-
-          setGpu({
-            name: gpu?.name,
-            load: Number(((gpu?.load ?? 1) * 100).toFixed(0)) ?? 0,
-            temperature: Number(gpu?.temperature?.toFixed(0)),
-            maxTemperature: Number(gpu?.maxTemperature),
-            frequency: Number(gpu?.frequency),
-            fan: Number(gpu?.fanSpeed),
-            watts: Number(gpu?.power.toFixed(0)),
-          });
-
-          setGpuWatts((prevGpuWatts) => {
-            const newGpuWatts = [
-              ...prevGpuWatts.slice(1),
-              Number(gpu?.power.toFixed(0)),
-            ];
-            return newGpuWatts;
-          });
-
-          setRam({
-            inUse: Math.round((ram.inUse ?? 1) / 1024),
-            inUsePercent: Math.round(
-              ((ram.inUse ?? 1) / (ram.totalSize ?? 1)) * 100
-            ),
-            totalSize: Math.round((ram.totalSize ?? 1) / 1024),
-          });
-
-          setKraken({
-            temperature: Number(kraken?.liquidTemperature.toFixed(0)),
-          });
-        },
+        onMonitoringDataUpdate: updateMonitoringData,
       },
     };
   }, []);
 
-  return { cpu, gpu, ram, kraken, cpuWatts, gpuWatts };
+  return monitoringData;
 };
 
 export default useMonitoring;
