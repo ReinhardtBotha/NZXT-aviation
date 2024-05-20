65fd70f73080ee134fad99856309ad7fa68502b3
Bruno Andrade <bruno.faria.andrade@gmail.com>
2024-05-20 16:52:31 -0300

    fix: fix useMonitoring hook (#2)

--- src/hooks/useMonitoring.jsx
+++ src/hooks/useMonitoring.jsx
@@ -1,84 +1,81 @@
-import React from "react";
+import React from 'react'
 
 const useMonitoring = () => {
-  const [monitoringData, setMonitoringData] = React.useState({
-    cpu: null,
-    gpu: null,
-    ram: null,
-    kraken: null,
-    cpuWatts: Array.from({ length: 60 }, (_, index) => index + 1),
-    gpuWatts: Array.from({ length: 60 }, (_, index) => index + 1),
-  });
+  const [cpu, setCpu] = React.useState(null)
+  const [gpu, setGpu] = React.useState(null)
+  const [ram, setRam] = React.useState(null)
+  const [kraken, setKraken] = React.useState(null)
+  const [cpuWatts, setCpuWatts] = React.useState(
+    Array.from({ length: 60 }, (_, index) => index + 1),
+  )
+  const [gpuWatts, setGpuWatts] = React.useState(
+    Array.from({ length: 60 }, (_, index) => index + 1),
+  )
 
   React.useEffect(() => {
-    const nzxtDefaults = window.nzxt?.v1;
+    const nzxtDefaults = window.nzxt?.v1
 
-    const updateMonitoringData = (data) => {
-      const { cpus, gpus, ram, kraken } = data;
+    const updateMonitoringData = data => {
+      const { cpus, gpus, ram, kraken } = data
 
-      const cpu = cpus.pop();
+      const cpu = cpus.pop()
 
-      let gpu;
+      let gpu
 
       if (gpus.length > 1) {
-        const [mainGpu] = gpus.filter(
-          (card) => !card.name.includes("Graphics")
-        );
-        gpu = mainGpu;
+        const [mainGpu] = gpus.filter(card => !card.name.includes('Graphics'))
+        gpu = mainGpu
       } else {
-        gpu = gpus.pop();
+        gpu = gpus.pop()
       }
 
-      setMonitoringData((prevData) => ({
-        ...prevData,
-        cpu: {
-          // name: cpu?.name,
-          load: Number(((cpu?.load ?? 1) * 100).toFixed(0)) ?? 0,
-          temperature: Number(cpu?.temperature?.toFixed(0)),
-          // maxTemperature: Number(cpu?.maxTemperature),
-          frequency: Number(cpu?.frequency),
-          fan: Number(cpu?.fanSpeed),
-        },
-        cpuWatts: [
-          ...prevData.cpuWatts.slice(1),
-          Number(cpu?.power.toFixed(0)),
-        ],
-        gpu: {
-          // name: gpu?.name,
-          load: Number(((gpu?.load ?? 1) * 100).toFixed(0)) ?? 0,
-          temperature: Number(gpu?.temperature?.toFixed(0)),
-          // maxTemperature: Number(gpu?.maxTemperature),
-          frequency: Number(gpu?.frequency),
-        },
-        gpuWatts: [
-          ...prevData.gpuWatts.slice(1),
-          Number(gpu?.power.toFixed(0)),
-        ],
-        ram: {
-          inUse: Math.round((ram.inUse ?? 1) / 1024),
-          inUsePercent: Math.round(
-            ((ram.inUse ?? 1) / (ram.totalSize ?? 1)) * 100
-          ),
-          totalSize: Math.round((ram.totalSize ?? 1) / 1024),
-        },
-        kraken: {
-          temperature: Number(kraken?.liquidTemperature.toFixed(0)),
-        },
-      }));
-    };
+      setCpu({
+        load: Number(((cpu?.load ?? 1) * 100).toFixed(0)) ?? 0,
+        temperature: Number(cpu?.temperature?.toFixed(0)),
+        frequency: Number(cpu?.frequency),
+        fan: Number(cpu?.fanSpeed),
+      })
+
+      setCpuWatts([...cpuWatts.slice(1), Number(cpu?.power.toFixed(0))])
+
+      setGpuWatts([...gpuWatts.slice(1), Number(gpu?.power.toFixed(0))])
+
+      setGpu({
+        load: Number(((gpu?.load ?? 1) * 100).toFixed(0)) ?? 0,
+        temperature: Number(gpu?.temperature?.toFixed(0)),
+        frequency: Number(gpu?.frequency),
+      })
+
+      setRam({
+        inUse: Math.round((ram.inUse ?? 1) / 1024),
+        inUsePercent: Math.round(((ram.inUse ?? 1) / (ram.totalSize ?? 1)) * 100),
+        totalSize: Math.round((ram.totalSize ?? 1) / 1024),
+      })
+
+      setKraken({
+        temperature: Number(kraken?.liquidTemperature.toFixed(0)),
+      })
+    }
 
     window.nzxt = {
       v1: {
         width: nzxtDefaults?.width ?? 0,
         height: nzxtDefaults?.height ?? 0,
-        shape: nzxtDefaults?.shape ?? "circle",
+        shape: nzxtDefaults?.shape ?? 'circle',
         targetFps: nzxtDefaults?.targetFps ?? 10,
         onMonitoringDataUpdate: updateMonitoringData,
       },
-    };
-  }, []);
+    }
+  }, [cpuWatts, gpuWatts])
 
-  return monitoringData;
-};
+  return {
+    cpu,
+    gpu,
+    ram,
+    kraken,
+    cpuWatts,
+    gpuWatts,
+  }
+}
 
-export default useMonitoring;
+export default useMonitoring
